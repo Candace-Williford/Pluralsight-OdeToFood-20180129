8ea332eeae7347ebdfdce47ed094fdbd0ba7887d
Candace Williford <candaceaw10@gmail.com>
2018-01-29 23:17:08 -0500

    These didn't get added to the commit

---
+++ controllers/HomeController.cs
@@ -0,0 +1,10 @@
+namespace OdeToFood.Controllers
+{
+    public class HomeController //receives request for root of the application
+    {
+        public string Index() //checks for this method
+        {
+            return "Hello from the home controller.";
+        }
+    }
+}
